4b23938d98463c25762d7a7111e56503818823f2
Hoài Châu <tranhoaichau.00@gmail.com>
2022-02-07 11:55:11 +0700

    implement exercises

---
+++ FileHandling/src/03/demos/03_Reading-Writing-Text-Files/src/org/paumard/files/ReadingFiles.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ReadingFiles {
+
+	public static void main(String[] args) {
+
+		Path path = Path.of("demos/03_Reading-Writing-Text-Files/files/sonnet-ISO.txt");
+		
+		boolean exists = Files.exists(path);
+		System.out.println("Exists = " + exists);
+		
+		try (BufferedReader reader = 
+				Files.newBufferedReader(path, StandardCharsets.ISO_8859_1);) {
+			
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println("Line = " + line);
+				line = reader.readLine();
+			}
+			
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
+
+
+
